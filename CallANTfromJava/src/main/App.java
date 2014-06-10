f3926edc221c16eb9025c4a4ce7ee44aeb28754c
Hierro <agaro1121@gmail.com>
2014-06-09 23:35:07 -0400

    Set property

--- CallANTfromJava/src/main/App.java
+++ CallANTfromJava/src/main/App.java
@@ -14,38 +14,39 @@ import org.apache.tools.ant.taskdefs.Expand;
 public class App{
 
 	public static void main(String[] args) throws FileNotFoundException {
-		 File buildFile = new File("sampleANT.xml");
-		   Project p = new Project();
-		   p.setUserProperty("ant.file", buildFile.getAbsolutePath());
-		   DefaultLogger consoleLogger = getConsoleLogger();
-		  
-		   p.addBuildListener(consoleLogger);
-		   p.addBuildListener(getFileLogger());
-		   
-		   p.init();
-		   ProjectHelper helper = ProjectHelper.getProjectHelper();
-		   p.addReference("ant.projectHelper", helper);
-		   helper.parse(p, buildFile);
-		   
-		   p.executeTarget("myTarget");
-		   p.fireBuildFinished(null);////says "BUILD SUCCESSFUL.."
+		File buildFile = new File("sampleANT.xml");
+		Project p = new Project();
+		p.setUserProperty("ant.file", buildFile.getAbsolutePath());
+		
+		p.setProperty("test", "hello world");
+		
+		DefaultLogger consoleLogger = getConsoleLogger();
+
+		p.addBuildListener(consoleLogger);
+		p.addBuildListener(getFileLogger());
+
+		p.init();
+		ProjectHelper helper = ProjectHelper.getProjectHelper();
+		p.addReference("ant.projectHelper", helper);
+		helper.parse(p, buildFile);
+
+		p.executeTarget("myTarget");
+		p.fireBuildFinished(null);////says "BUILD SUCCESSFUL.."
 	}
 
 	private static DefaultLogger getConsoleLogger() {
-        DefaultLogger consoleLogger = new DefaultLogger();
-        consoleLogger.setErrorPrintStream(System.err);
-        consoleLogger.setOutputPrintStream(System.out);
-        consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
-         
-        return consoleLogger;
-    }
-	
+		DefaultLogger consoleLogger = new DefaultLogger();
+		consoleLogger.setErrorPrintStream(System.err);
+		consoleLogger.setOutputPrintStream(System.out);
+		consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
+		return consoleLogger;
+	}
+
 	private static DefaultLogger getFileLogger() throws FileNotFoundException{
 		DefaultLogger fileLogger = new DefaultLogger();
 		fileLogger.setErrorPrintStream(System.err);
 		fileLogger.setOutputPrintStream(new PrintStream("log_test"));
 		fileLogger.setMessageOutputLevel(Project.MSG_INFO);
 		return fileLogger;
-		
 	}
 }
